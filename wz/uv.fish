b9b04999e2bb1af791e28d2fbafc0c7216cbcb03
winiyaz <winiyaz@proton.me>
2024-09-23 03:42:09 +0000

    .

--- wz/uv.fish
+++ wz/uv.fish
@@ -1,19 +1,9 @@
-#!/bin/fish
+#!/usr/bin/env fish 
 echo " "
+echo " Install UV and Fish Autocomplete "
 echo " "
-echo " This will install uv python manager "
-echo " Main Website - https://docs.astral.sh/uv/"
-echo " "
-echo " Main Install"
-echo " --------------------"
-echo " curl -LsSf https://astral.sh/uv/install.sh | sh "
-echo ""
-echo ""
+# Install UV 
 curl -LsSf https://astral.sh/uv/install.sh | sh
-echo ""
-echo " Now setting up autocomplete with fish"
-echo " echo 'uv generate-shell-completion fish | source' >> ~/.config/fish/config.fish  "
-echo " echo 'uvx --generate-shell-completion fish | source' >> ~/.config/fish/config.fish "
+# Setup automcomplete 
 echo 'uv generate-shell-completion fish | source' >> ~/.config/fish/config.fish
 echo 'uvx --generate-shell-completion fish | source' >> ~/.config/fish/config.fish
-echo "Now exit out of shell and try again "
