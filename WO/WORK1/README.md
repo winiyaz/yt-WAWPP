10c14fdc11e405c9624e3988bedf729dd6edcbad
winiyaz <winiyaz@proton.me>
2024-09-23 12:45:31 +0000

    .

---
+++ WO/WORK1/README.md
@@ -0,0 +1,7 @@
+
+1. [WORK1](#work1)
+
+
+# WORK1
+
+1. Version 1 of doing this work 
--- WO/WORK1/main.py
+++
@@ -1,9 +0,0 @@
-# Entry point into application actual code will be in its own subdirectory
-
-
-def panty():
-    print("Hello from work1!")
-
-
-if __name__ == "__main__":
-    panty()
