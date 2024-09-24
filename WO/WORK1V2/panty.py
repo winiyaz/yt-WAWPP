cf394474b64fe1dc7098cbe3f66c4b0f044a6a4f
winiyaz <winiyaz@proton.me>
2024-09-24 14:00:48 +0000

    .

--- WO/WORK1V2/panty.py
+++ WO/WORK1V2/panty.py
@@ -1,12 +1,13 @@
 # Entry point into application actual code will be in its own subdirectory
 from src.p1 import m_F1
+from src.p2 import m_F2
 from src.ban import banner
 
 
 def main():
     print("Hello from work1!")
-    banner("Running m_F1 getting screnshot in clics directory")
-    m_F1()
+    banner("Running m_F2 Using DIR as a list")
+    m_F2()
 
 
 if __name__ == "__main__":
