7f66850ca7450e50925ecffef4f725d32e09dc84
winiyaz <winiyaz@proton.me>
2024-09-24 15:34:03 +0000

    .

--- WO/WORK1-V3/panty.py
+++ WO/WORK1-V3/panty.py
@@ -2,6 +2,7 @@
 from src.p1 import m_F1
 from src.p2 import m_F2
 from src.p3 import m_F3
+from src.p4 import m_F4
 from src.ban import banner
 from rich.traceback import install
 
@@ -11,7 +12,7 @@ install(show_locals=True)
 def main():
     print("Hello from work1!")
     banner("Running m_F3 Using DIR as a list \n Looping through list of website")
-    m_F3
+    m_F4()
 
 
 if __name__ == "__main__":
---
+++ WO/WORK1-V3/src/p4.py
@@ -0,0 +1,61 @@
+from playwright.sync_api import sync_playwright
+from datetime import datetime, timezone
+import time
+import os
+
+# Target and userAgent
+urls = [
+    "https://www.coingecko.com/",
+    "https://coinswatch.com/",
+    "https://www.meanbitches.com/",
+]
+uA = "Mozilla/5.0 (Linux; Android 11; Redmi Note 8 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36"
+
+
+def m_F4():
+    """All operations will be in this function"""
+    with sync_playwright() as p:
+        # Launch browser
+        br1 = p.chromium.launch()
+
+        # Loop through the URLs
+        for index, url in enumerate(urls):
+            # Create a new browser context
+            c_d = datetime.now(timezone.utc).strftime("%Y%m%d_%H%M%S")
+            context_config = {
+                "record_video_dir": "clicks/",
+                "record_video_size": {"width": 640, "height": 480},
+                "user_agent": uA,
+                "locale": "de-DE",
+                "timezone_id": "Europe/Berlin",
+            }
+            context = br1.new_context(**context_config)
+
+            # Create a new page
+            page1 = context.new_page()
+
+            # Visit the page specified
+            page1.goto(url)
+
+            # Create screenshot
+            page1.screenshot(path=f"clicks/{c_d}-s{index+1}.png", full_page=True)
+
+            # Close the page
+            page1.close()
+
+            # Close the context
+            context.close()
+
+            # Wait for 2 seconds to allow the video file to be written
+            time.sleep(2)
+
+            # Rename the video file
+            for filename in os.listdir("clicks/"):
+                if filename.startswith("video-") and filename.endswith(".webm"):
+                    os.rename(f"clicks/{filename}", f"clicks/{c_d}-video{index+1}.webm")
+
+        # Close the browser
+        br1.close()
+
+
+m_F4()
